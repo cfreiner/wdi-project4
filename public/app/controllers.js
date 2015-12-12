193fe965cf5e305895ca6d9e4203790b3dfd56f0
Carl Reiner <creiner1@gmail.com>
2015-12-11 23:24:39 -0800

    added search view

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,8 +1,9 @@
 angular.module('WikiCtrls', ['WikiServices'])
   .controller('SearchCtrl', ['$scope','Article', function($scope, Article) {
+    $scope.results = '';
     $scope.search = function(q) {
       Article.query({q:q}, function(data) {
-
-      })
-    }
+        $scope.results = data;
+      });
+    };
   }]);
