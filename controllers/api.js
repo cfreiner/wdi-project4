7f3b955a1304c858532b5fb4b1e807131e5ed18c
Carl Reiner <creiner1@gmail.com>
2015-12-13 19:23:45 -0800

    added functions for sentiment analysis

--- controllers/api.js
+++ controllers/api.js
@@ -14,7 +14,42 @@ router.get('/search', function(req, res) {
 });
 
 router.get('/test', function(req, res) {
+  var s = processSentiment('Hello hello good Good bad bad bad Bad evil Evil happy');
+  res.send(s);
+});
 
-})
+function processSentiment(text) {
+  var analyzed = sentiment(text);
+  var output = {
+    score: analyzed.score,
+    comparative: analyzed.comparative.toFixed(2),
+    words: []
+  };
+  addWords(analyzed.positive, output.words, 'positive');
+  addWords(analyzed.negative, output.words, 'negative');
+  return output;
+}
+
+function addWords(inputArr, outputArr, valence) {
+  for(var i = 0; i < inputArr.length; i++) {
+    var found = false;
+    for(var j = 0; j < outputArr.length; j++) {
+      if(inputArr[i] === outputArr[j].word) {
+        outputArr[j].frequency += 1;
+        found = true;
+        break;
+      }
+    }
+    if(!found) {
+      outputArr.push(new WordNode(inputArr[i], valence));
+    }
+  }
+}
+
+function WordNode(word, valence) {
+  this.word = word;
+  this.valence = valence;
+  this.frequency = 1;
+}
 
 module.exports = router;
