d93212582b72f77ec2eb58126ea7324a9e173963
Carl Reiner <creiner1@gmail.com>
2015-12-14 10:14:50 -0800

    added circle packing layout directive

--- public/app/d3-directives.js
+++ public/app/d3-directives.js
@@ -7,6 +7,59 @@ angular.module('D3Directives', ['D3Services'])
       },
       link: function(scope, element, attrs) {
         d3.d3().then(function(d3) {
+          var svg = d3.select(element[0])
+            .append('svg')
+            .style('width', '100%');
+
+          //Make the watch function below work
+          window.onresize = function() {
+            scope.$apply();
+          };
+
+          //Watch for window resize, re-render on change
+          scope.$watch(function() {
+            return angular.element($window)[0].innerWidth;
+          }, function() {
+            scope.render(scope.data);
+          });
+
+          scope.render = function(data) {
+
+            //Clear the existing SVG
+            svg.selectAll('*').remove();
+
+            var diameter = window.innerWidth/2;
+
+            var bubble = d3.layout.pack()
+              .sort(null)
+              .size([diameter, diameter])
+              .padding(15);
+
+            var svg = d3.select("#vis").append("svg")
+              .attr("width", diameter)
+              .attr("height", diameter)
+              .attr("class", "bubble");
+
+            var node = svg.selectAll(".node")
+              .data(bubble.nodes({children: tokens}))
+              .enter()
+              .append("g")
+              .attr("class", "node")
+              .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+            node.append("title")
+              .text(function(d) { return d.name; });
+
+            node.append("circle")
+              .attr("r", function(d) { return d.r; })
+              .attr("class", function(d) { return d.connotation; });
+
+            node.append("text")
+              .text(function(d) { return d.name; })
+              .style("font-size", function(d) { return (d.r/2); })
+              .style("text-anchor", "middle");
+
+          };
 
         });
       }
