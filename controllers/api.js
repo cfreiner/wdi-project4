7ccf5f02921f2b849c79bd28c193211e9922b46d
Carl Reiner <creiner1@gmail.com>
2015-12-14 17:12:52 -0800

    moved some helper functions into the scope of the get route

--- controllers/api.js
+++ controllers/api.js
@@ -10,6 +10,8 @@ moongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/wiki');
 var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
 
 router.get('/search', function(req, res) {
+  var duplicateSearch = searchExists() ? true : false;
+
   request(url + req.query.q, function(err, response, body) {
     if(!err && response.statusCode == 200) {
       var text = getWikiText(JSON.parse(body));
@@ -17,7 +19,66 @@ router.get('/search', function(req, res) {
       res.send(processSentiment(text));
     }
   });
+
+  //Helper function to tally the frequencies of an array of words.
+  //Packages them into WordNode objects and adds them to Mongo if necessary.
+  function addWords(inputArr, outputArr, valence) {
+    for(var i = 0; i < inputArr.length; i++) {
+      var found = false;
+      if(!duplicateSearch) {
+        incrementMongoWord(inputArr[i], valence);
+      }
+      for(var j = 0; j < outputArr.length; j++) {
+        if(inputArr[i] === outputArr[j].word) {
+          outputArr[j].value += 1;
+          found = true;
+          break;
+        }
+      }
+      if(!found) {
+        outputArr.push(new WordNode(inputArr[i], valence));
+      }
+    }
+  }
+
+  //Return an object of word frequencies
+  function processSentiment(text) {
+    var analyzed = sentiment(text);
+    var output = {
+      score: analyzed.score,
+      comparative: analyzed.comparative.toFixed(2),
+      words: []
+    };
+    addWords(analyzed.positive, output.words, 'positive');
+    addWords(analyzed.negative, output.words, 'negative');
+    return output;
+  }
+
 });
+
+function searchExists(searchTerm) {
+  Search.findOne({search: searchTerm}, function(err, result) {
+    if (err) {
+      console.log(err);
+      return true;
+    } else if(result) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+//Pull just the content of the Wikipedia page
+function getWikiText(wikiJSON) {
+  result = '';
+  var pages = wikiJSON.query.pages;
+  for(var key in pages) {
+    result += pages[key].revisions[0]['*'];
+  }
+  return result;
+}
+
 
 function incrementMongoWord(inputWord, valence) {
   Word.findOne({word: inputWord}, function(err, word) {
@@ -43,47 +104,6 @@ function incrementMongoWord(inputWord, valence) {
       }
     }
   });
-}
-
-//Pull just the content of the Wikipedia page
-function getWikiText(wikiJSON) {
-  result = '';
-  var pages = wikiJSON.query.pages;
-  for(var key in pages) {
-    result += pages[key].revisions[0]['*'];
-  }
-  return result;
-}
-
-//Return an object of work frequencies
-function processSentiment(text) {
-  var analyzed = sentiment(text);
-  var output = {
-    score: analyzed.score,
-    comparative: analyzed.comparative.toFixed(2),
-    words: []
-  };
-  addWords(analyzed.positive, output.words, 'positive');
-  addWords(analyzed.negative, output.words, 'negative');
-  return output;
-}
-
-//Tally the frequencies of an array of words.
-//Package them into WordNode objects
-function addWords(inputArr, outputArr, valence) {
-  for(var i = 0; i < inputArr.length; i++) {
-    var found = false;
-    for(var j = 0; j < outputArr.length; j++) {
-      if(inputArr[i] === outputArr[j].word) {
-        outputArr[j].value += 1;
-        found = true;
-        break;
-      }
-    }
-    if(!found) {
-      outputArr.push(new WordNode(inputArr[i], valence));
-    }
-  }
 }
 
 //Class for storing words and frequencies
