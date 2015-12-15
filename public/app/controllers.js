03afe2ef5a3f7d4b32e95513b658cf38b98a64cd
Carl Reiner <creiner1@gmail.com>
2015-12-15 14:51:13 -0800

    added score to search schema

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,9 +1,12 @@
 angular.module('WikiCtrls', ['WikiServices', 'D3Directives'])
   .controller('SearchCtrl', ['$scope', 'Article', '$window', function($scope, Article, $window) {
-    $scope.results = '';
+    $scope.score;
+    $scope.title;
     $scope.search = function(q) {
+      $scope.title = 'Article: ' + q;
       Article.get({q:q}, function(data) {
         $scope.data = data.words;
+        $scope.score = 'Score: ' + data.score;
       });
     };
   }])
