7c96dca7c9d225f90bc66a0cbbda8da2e3c5ec7e
Carl Reiner <creiner1@gmail.com>
2015-12-14 20:31:25 -0800

    added async eachSeries in an attempt to synchronize crap

--- controllers/api.js
+++ controllers/api.js
@@ -5,9 +5,38 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Word = require('../models/word');
 var Search = require('../models/search');
+var async = require('async');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/wiki');
 var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+
+//Helper function to update the Mongo database when processing words
+function incrementMongoWord(inputWord, valence) {
+  Word.findOne(function(err, word) {
+    console.log(word);
+    if(err) {
+      console.log(err);
+    } else {
+      if(word) {
+        console.log(word);
+        Word.findByIdAndUpdate(word.id, {value: word.value + 1}, function(err, word) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log('Updated: ', word.word, word.value);
+          }
+        });
+      } else {
+        var newWord = new Word(new WordNode(inputWord, valence));
+        newWord.save(function(err) {
+          if (err) {
+            return res.send(err);
+          }
+        });
+      }
+    }
+  });
+}
 
 router.get('/search', function(req, res) {
   var duplicateSearch = searchExists() ? true : false;
@@ -17,7 +46,46 @@ router.get('/search', function(req, res) {
     if(!err && response.statusCode == 200) {
       var text = getWikiText(JSON.parse(body));
       var processed = processSentiment(text);
-      res.send(processSentiment(text));
+
+      async.eachSeries(processed.words, function(item, callback) {
+        //Check for word in mongo
+        //If found, increment, if not, create
+        Word.findOne({word: item.word}, function(err, word) {
+          if(err) {
+            console.log(err);
+            callback(err);
+          } else {
+            if(word) {
+              console.log('Found word:', word);
+              Word.findByIdAndUpdate(word.id, {value: word.value + 1}, function(err, word) {
+                if(err) {
+                  console.log(err);
+                  callback(err);
+                } else {
+                  console.log('Updated: ', word.word, word.value);
+                  callback();
+                }
+              });
+            } else {
+              var newWord = new Word(new WordNode(inputWord, valence));
+              newWord.save(function(err) {
+                if (err) {
+                  return res.send(err);
+                }
+                callback();
+              });
+            }
+          }
+        });
+      }, function(error) {
+        if(error) {
+          console.log('Async Error: ', error);
+        } else {
+          console.log('Async series completed successfully.');
+        }
+      });
+
+      res.send(processed);
     }
   });
 
@@ -26,9 +94,6 @@ router.get('/search', function(req, res) {
   function addWords(inputArr, outputArr, valence) {
     for(var i = 0; i < inputArr.length; i++) {
       var found = false;
-      if(!duplicateSearch) {
-        incrementMongoWord(inputArr[i], valence);
-      }
       for(var j = 0; j < outputArr.length; j++) {
         if(inputArr[i] === outputArr[j].word) {
           outputArr[j].value += 1;
@@ -81,34 +146,6 @@ function getWikiText(wikiJSON) {
     result += pages[key].revisions[0]['*'];
   }
   return result;
-}
-
-//Helper function to update the Mongo database when processing words
-function incrementMongoWord(inputWord, valence) {
-  Word.findOne(function(err, word) {
-    console.log(word);
-    if(err) {
-      console.log(err);
-    } else {
-      if(word) {
-        console.log(word);
-        Word.findByIdAndUpdate(word.id, {value: word.value + 1}, function(err, word) {
-          if(err) {
-            console.log(err);
-          } else {
-            console.log('Updated: ', word.word, word.value);
-          }
-        });
-      } else {
-        var newWord = new Word(new WordNode(inputWord, valence));
-        newWord.save(function(err) {
-          if (err) {
-            return res.send(err);
-          }
-        });
-      }
-    }
-  });
 }
 
 //Class for storing words and frequencies
