17e19559137a7970c26e1334a719c6d16392e217
Carl Reiner <creiner1@gmail.com>
2015-12-15 16:48:06 -0800

    added versus controller

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,7 +1,5 @@
 angular.module('WikiCtrls', ['WikiServices', 'D3Directives'])
   .controller('SearchCtrl', ['$scope', 'Article', '$window', function($scope, Article, $window) {
-    $scope.score;
-    $scope.title;
     $scope.search = function(q) {
       $scope.title = 'Article: ' + q;
       Article.get({q:q}, function(data) {
@@ -18,4 +16,19 @@ angular.module('WikiCtrls', ['WikiServices', 'D3Directives'])
     }, function error(data) {
       console.log('Error: ', data);
     });
+  }])
+  .controller('VersusCtrl', ['$scope', 'Article', function($scope, Article) {
+    $scope.search = function(q, side) {
+      Article.get({q:q}, function(data) {
+        if(side === 'left') {
+          $scope.titleLeft = 'Article: ' + q;
+          $scope.dataLeft = data.words;
+          $scope.scoreLeft = 'Score: ' + data.score;
+        } else if(side === 'right') {
+          $scope.titleRight = 'Article: ' + q;
+          $scope.dataRight = data.words;
+          $scope.scoreRight = 'Score: ' + data.score;
+        }
+      });
+    };
   }]);
